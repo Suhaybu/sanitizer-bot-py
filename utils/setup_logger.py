233b7515669f238d7ef928e2ed09d4ad8c547957
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-01-20 00:29:20 +0300

    init

---
+++ utils/setup_logger.py
@@ -0,0 +1,26 @@
+import logging
+import sys
+
+
+def setup_logger():
+    # Initializing logger
+    logger = logging.getLogger('discord')
+    logger.setLevel(logging.INFO)
+
+    # File handler
+    handler = logging.FileHandler(
+        filename='logs/discord.log', encoding='utf-8', mode='w'
+    )
+    handler.setFormatter(
+        logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+    )
+    logger.addHandler(handler)
+
+    # Console handler
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(
+        logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+    )
+    logger.addHandler(console_handler)
+
+    return logger

f916be7b3d015093d6fa8fec8d9c0147b90069d4
Suhayb U <75957262+Suhaybu@users.noreply.github.com>
2024-01-19 10:54:24 +0300

    added sync command

---
+++ cogs/admin.py
@@ -0,0 +1,34 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    # $sync: sync slash commands (Prefix)
+    @commands.command(
+        name='sync',
+        aliases=['Sync'],
+        description='Requests all slash commands to sync.',
+    )
+    @commands.is_owner()
+    async def sync(self, ctx):
+        # ctx.send(
+        #     f'User {ctx.author.name} ({ctx.author.id}) used Prefix Sync command'
+        # )
+        synced = await self.bot.tree.sync()
+        print(f'Successfully synced {synced} command(s)')
+
+    # /sync: sync slash commands
+    # @app_commands.command(
+    #     name='sync', description='Requests all slash commands to sync.'
+    # )
+    # @app_commands.checks.is_owner()
+    # async def ssync(self, interaction: discord.Interaction):
+    #     ...
+
+
+async def setup(bot):
+    await bot.add_cog(Admin(bot))

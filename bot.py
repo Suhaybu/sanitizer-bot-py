12d4e049f568207ea33f5fcf5ef4f8e36230b4a8
Suhayb U <75957262+Suhaybu@users.noreply.github.com>
2024-01-19 10:18:12 +0300

    added cog loading logic added useful console messages added bot presence

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import discord
 from discord.ext import commands
 
 from settings import BOT_TOKEN
+from utils.cogs_loader import print_cog_status
 
 intents = discord.Intents.default()
 discord.Intents.message_content = True
@@ -15,26 +16,38 @@ bot = commands.Bot(
 
 
 async def load_cogs():
+    loaded_cogs = []
+    faulty_cogs = []
+
     for cog_name in os.listdir('./cogs'):
         if cog_name.endswith('.py'):
-            await bot.load_extension()
+            try:
+                await bot.load_extension(f'cogs.{cog_name[:-3]}')
+                loaded_cogs.append(cog_name[-3])
+            except Exception as e:
+                faulty_cogs.append(cog_name[:-3])
+
+    print_cog_status(loaded_cogs, faulty_cogs)
 
 
 async def main():
     async with bot:
+        print('(1/2) Starting the bot and loading cogs')
+        await load_cogs()
         await bot.start(BOT_TOKEN)
 
 
 @bot.event
 async def on_ready():
-    print('Sanitizer Bot is Online!')
+    print(
+        f'(2/2) Sanitizer Bot is Online! Discord version: {discord.__version__}'
+    )
     await bot.change_presence(
         activity=discord.Activity(
-            type=discord.ActivityType.watching,
-            name=f"!help",
+            type=discord.ActivityType.listening,
+            name=f"to !help",
         )
     )
-    print(discord.__version__)
 
 
 asyncio.run(main())

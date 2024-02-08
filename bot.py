35495c55268d3f04cae7176fed7ec830df6a703d
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-02-08 15:24:13 +0300

    removed unused variables + switched to tabs from spaces

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-'''
+"""
 This is the main file where the bot starts from.
-'''
+"""
 
 import asyncio
 import os
@@ -14,48 +14,46 @@ from utils.setup_logger import setup_logger
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or('!'), intents=intents
-)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents)
 
 logger = setup_logger()
 
 
 async def load_cogs():
-    loaded_cogs = []
-    faulty_cogs = []
+	loaded_cogs = []
+	faulty_cogs = []
 
-    for cog_name in os.listdir('./cogs'):
-        if cog_name.endswith('.py'):
-            try:
-                await bot.load_extension(f'cogs.{cog_name[:-3]}')
-                loaded_cogs.append(cog_name[:-3])
-            except Exception as e:
-                faulty_cogs.append(cog_name[:-3])
+	for cog_name in os.listdir('./cogs'):
+		if cog_name.endswith('.py'):
+			try:
+				await bot.load_extension(f'cogs.{cog_name[:-3]}')
+				loaded_cogs.append(cog_name[:-3])
+			except Exception:
+				faulty_cogs.append(cog_name[:-3])
 
-    print_cog_status(loaded_cogs, faulty_cogs)
+	print_cog_status(loaded_cogs, faulty_cogs)
 
 
 async def main():
-    async with bot:
-        print('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓')
-        print('┃           Bot is starting up and loading cogs...           ┃')
-        print('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛')
-        await load_cogs()
-        await bot.start(BOT_TOKEN)
+	async with bot:
+		print('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓')
+		print('┃           Bot is starting up and loading cogs...           ┃')
+		print('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛')
+		await load_cogs()
+		await bot.start(BOT_TOKEN)
 
 
 @bot.event
 async def on_ready():
-    print('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓')
-    print('┃                  Sanitizer Bot is Online!                  ┃')
-    print('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛')
-    await bot.change_presence(
-        activity=discord.Activity(
-            type=discord.ActivityType.listening,
-            name=f"!help",
-        )
-    )
+	print('┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓')
+	print('┃                  Sanitizer Bot is Online!                  ┃')
+	print('┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛')
+	await bot.change_presence(
+		activity=discord.Activity(
+			type=discord.ActivityType.listening,
+			name='!help',
+		)
+	)
 
 
 asyncio.run(main())

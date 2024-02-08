b9dcd9427b06bf3db400fa22d278f588722521e7
Suhayb <75957262+Suhaybu@users.noreply.github.com>
2024-02-08 15:24:31 +0300

    switched to tabs from spaces

--- utils/setup_logger.py
+++ utils/setup_logger.py
@@ -3,24 +3,24 @@ import sys
 
 
 def setup_logger():
-    # Initializing logger
-    logger = logging.getLogger('discord')
-    logger.setLevel(logging.INFO)
+	# Initializing logger
+	logger = logging.getLogger('discord')
+	logger.setLevel(logging.INFO)
 
-    # File handler
-    handler = logging.FileHandler(
-        filename='logs/discord.log', encoding='utf-8', mode='w'
-    )
-    handler.setFormatter(
-        logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
-    )
-    logger.addHandler(handler)
+	# File handler
+	handler = logging.FileHandler(
+		filename='logs/discord.log', encoding='utf-8', mode='w'
+	)
+	handler.setFormatter(
+		logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+	)
+	logger.addHandler(handler)
 
-    # Console handler
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(
-        logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
-    )
-    logger.addHandler(console_handler)
+	# Console handler
+	console_handler = logging.StreamHandler(sys.stdout)
+	console_handler.setFormatter(
+		logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+	)
+	logger.addHandler(console_handler)
 
-    return logger
+	return logger

059c1e0fb55c546788b97e2c8f3fc05a2093711a
Suhayb U <75957262+Suhaybu@users.noreply.github.com>
2024-02-12 01:42:36 +0300

    init

--- cogs/general.py
+++ cogs/general.py
@@ -5,6 +5,16 @@ class General(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
+	@commands.hybrid_command(name='credits', description='Roll the credits!')
+	async def credits(self, ctx):
+		if ctx.author == self.bot.user:
+			return
+
+		with open('credits.md', 'r', encoding='utf-8') as file:
+			bot_response = file.read()
+
+		await ctx.reply(bot_response, ephemeral=True)
+
 
 async def setup(bot):
 	await bot.add_cog(General(bot))

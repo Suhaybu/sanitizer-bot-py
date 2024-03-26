b048b3aeee3fdb3e783b3b9e8ea3dbbef0f18a6a
Suhayb Ullah <75957262+Suhaybu@users.noreply.github.com>
2024-03-26 09:30:27 +0300

    remade regex to handel ignoring text unrelated to the link, added ignorecase and multiline, regex will now be compiled

--- exts/instagram.py
+++ exts/instagram.py
@@ -7,7 +7,12 @@ from interactions.api.events import MessageCreate
 class Instagram(Extension):
 	@listen(event_name=MessageCreate)
 	async def on_message(self, event: MessageCreate):
-		instagram_regex = r'https?:\/\/(?:www\.)?instagram\.com/(reel|p)/([^?]+)'
+		# instagram_regex = r'https?:\/\/(?:www\.)?instagram\.com/(reel|p)/([^?]+)'
+		instagram_regex = re.compile(
+			r'https?:\/\/(?:www\.)?instagram\.com/(reel|p)/([^/\s?]+)',
+			re.IGNORECASE | re.MULTILINE,
+		)
+
 		match = re.search(instagram_regex, event.message.content)
 
 		if match:
@@ -17,7 +22,7 @@ class Instagram(Extension):
 			elif type == 'p':
 				type_printed = 'Post'
 
-			bot_response = f'[{type_printed} via Instagram](https://www.ddinstagram.com/{type}/{url_data})'
+			bot_response = f'[{type_printed} via Instagram](https://www.ddinstagram.com/{type}/{url_data}/)'
 			# no_reply = AllowedMentions.none()
 			await event.message.add_reaction('<:Sanitized:1206376642042138724>')
 			await event.message.reply(

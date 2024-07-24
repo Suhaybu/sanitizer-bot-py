db75328fa79cea63d05e1a17a1318d0e07831ed3
Suhayb Ullah <75957262+Suhaybu@users.noreply.github.com>
2024-07-24 17:35:16 +0300

    now returns an embed instead of text

--- exts/credits.py
+++ exts/credits.py
@@ -1,4 +1,4 @@
-from interactions import Extension, SlashContext, slash_command
+from interactions import Embed, Extension, SlashContext, slash_command
 
 
 class Credits(Extension):
@@ -6,4 +6,4 @@ class Credits(Extension):
 	async def credits(self, ctx: SlashContext):
 		with open(file="credits.md", mode="r", encoding="utf-8") as file:
 			bot_response = file.read()
-		await ctx.send(content=bot_response, ephemeral=True, suppress_embeds=True)
+		await ctx.send(embed=Embed(description=bot_response), ephemeral=True)

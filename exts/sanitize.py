04254ad1bb66364f208eeec1285c9244c6fd913a
Suhayb Ullah <75957262+Suhaybu@users.noreply.github.com>
2024-08-05 23:30:22 +0300

    Implemented basic checking for URL's, TODO: Need to refactor code and remove boilerplate

--- exts/sanitize.py
+++ exts/sanitize.py
@@ -6,9 +6,11 @@ from interactions import (AllowedMentions, ContextMenuContext, Embed,
                           slash_command, slash_option)
 from interactions.api.events import MessageCreate
 
+from models.enums.service import Service, ServiceResponse
 from socialmedia_handlers.instagram import Instagram
 from socialmedia_handlers.tiktok import Tiktok
 from socialmedia_handlers.twitter import Twitter
+from utils.bot_response_checker import BotResponseChecker
 
 
 class Sanitize(Extension):
@@ -17,7 +19,7 @@ class Sanitize(Extension):
 		self.tiktok = Tiktok()
 		self.twitter = Twitter()
 
-	async def sanitize(self, user_input: str) -> str | None:
+	async def sanitize(self, user_input: str) -> ServiceResponse | None:
 		instagram_response = self.instagram.get_response(user_input)
 		tiktok_response = self.tiktok.get_response(user_input)
 		twitter_response = self.twitter.get_response(user_input)
@@ -25,21 +27,58 @@ class Sanitize(Extension):
 		instagram_result, tiktok_result, twitter_result = await asyncio.gather(
 			instagram_response, tiktok_response, twitter_response
 		)
-		return instagram_result or tiktok_result or twitter_result
+
+		if instagram_result:
+			return ServiceResponse(Service.INSTAGRAM, instagram_result)
+		elif tiktok_result:
+			return ServiceResponse(Service.TIKTOK, tiktok_result)
+		elif twitter_result:
+			return ServiceResponse(Service.TWITTER, twitter_result)
+		else:
+			return None
 
 	# Listener for automatically responding to valid messages
 	@listen(event_name=MessageCreate)
 	async def on_message(self, event: MessageCreate):
 		try:
-			bot_response = await self.sanitize(user_input=event.message.content)
+			response = await self.sanitize(user_input=event.message.content)
 
-			if not bot_response:
+			if not response:
 				return
 
 			await event.message.add_reaction("<:Sanitized:1206376642042138724>")
-			await event.message.reply(bot_response, allowed_mentions={"parse": []})
-			if isinstance(event.message.author, Member):
+			bot_message = await event.message.reply(
+				response.content, allowed_mentions={"parse": []}
+			)
+
+			for _ in range(10):
+				await asyncio.sleep(1)
+				if bot_message.embeds:
+					break
+
+			is_valid_response = BotResponseChecker.check_response(
+				bot_message.embeds, response.service
+			)
+
+			if isinstance(event.message.author, Member) and is_valid_response:
 				await event.message.suppress_embeds()
+
+			if not is_valid_response:
+				await event.message.remove_reaction("<:Sanitized:1206376642042138724>")
+				await bot_message.delete()
+				response = (
+					"Something went wrong. The service I rely on for fixing \n"
+					f"{response.service.value} links seems to be down. Please try again later!"
+				)
+				error_message = await event.message.reply(
+					embed=Embed(
+						title="Sorry :c", description=response, color="#d1001f"
+					),
+					allowed_mentions={"parse": []},
+				)
+
+				await asyncio.sleep(10)
+				await error_message.delete()
 
 		except Exception:
 			return
@@ -55,22 +94,45 @@ class Sanitize(Extension):
 	@integration_types(guild=True, user=True)
 	async def slash_sanitize(self, ctx: SlashContext, link: str):
 		try:
-			bot_response = await self.sanitize(user_input=link)
+			response = await self.sanitize(user_input=link)
 
-			if not bot_response:
-				bot_response = (
+			if not response:
+				response = (
 					"I couldn't find a supported URL in the message you asked me to sanitize.\n"
 					"To learn more about me and what I can do, try `/credits`! "
 				)
 				await ctx.send(
 					embed=Embed(
-						title="Sorry :c", description=bot_response, color="#d1001f"
+						title="Sorry :c", description=response, color="#d1001f"
 					),
 					ephemeral=True,
 				)
 				return
 
-			await ctx.send(bot_response, allowed_mentions=AllowedMentions.none())
+			bot_message = await ctx.send(
+				response.content, allowed_mentions=AllowedMentions.none()
+			)
+
+			for _ in range(10):
+				await asyncio.sleep(1)
+				if bot_message.embeds:
+					break
+
+			is_valid_response = BotResponseChecker.check_response(
+				bot_message.embeds, response.service
+			)
+			if not is_valid_response:
+				await bot_message.delete()
+				response = (
+					"Something went wrong. The service I rely on for fixing \n"
+					f"{response.service.value} links seems to be down. Please try again later!"
+				)
+				await ctx.send(
+					embed=Embed(
+						title="Sorry :c", description=response, color="#d1001f"
+					),
+					ephemeral=True,
+				)
 
 		except Exception:
 			return
@@ -81,22 +143,45 @@ class Sanitize(Extension):
 	async def context_sanitize(self, ctx: ContextMenuContext):
 		message: Message = ctx.target
 		try:
-			bot_response = await self.sanitize(user_input=message.content)
+			response = await self.sanitize(user_input=message.content)
 
-			if not bot_response:
-				bot_response = (
+			if not response:
+				response = (
 					"I couldn't find a supported URL in the message you asked me to sanitize.\n"
 					"To learn more about me and what I can do, try `/credits`! "
 				)
 				await ctx.send(
 					embed=Embed(
-						title="Sorry :c", description=bot_response, color="#d1001f"
+						title="Sorry :c", description=response, color="#d1001f"
 					),
 					ephemeral=True,
 				)
 				return
 
-			await ctx.send(bot_response, allowed_mentions=AllowedMentions.none())
+			bot_message = await ctx.send(
+				response.content, allowed_mentions=AllowedMentions.none()
+			)
+
+			for _ in range(10):
+				await asyncio.sleep(1)
+				if bot_message.embeds:
+					break
+
+			is_valid_response = BotResponseChecker.check_response(
+				bot_message.embeds, response.service
+			)
+			if not is_valid_response:
+				await bot_message.delete()
+				response = (
+					"Something went wrong. The service I rely on for fixing \n"
+					f"{response.service.value} links seems to be down. Please try again later!"
+				)
+				await ctx.send(
+					embed=Embed(
+						title="Sorry :c", description=response, color="#d1001f"
+					),
+					ephemeral=True,
+				)
 
 		except Exception:
 			return

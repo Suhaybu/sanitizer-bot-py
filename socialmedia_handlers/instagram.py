96533f8f7872a027c12535160fe0dd58baf9c90c
Suhayb Ullah <75957262+Suhaybu@users.noreply.github.com>
2024-08-05 21:01:40 +0300

    now uses abstracted quickvids_handler.py first, and then fallbacks to ddinstagram.com

--- socialmedia_handlers/instagram.py
+++ socialmedia_handlers/instagram.py
@@ -1,5 +1,11 @@
 import re
 
+import aiohttp
+
+from models.enums.service import Service
+from models.errors import APIError, NoAPIResponseError
+from utils.quickvids_handler import QuickvidsHandler
+
 
 class Instagram:
 	def __init__(self) -> None:
@@ -7,6 +13,7 @@ class Instagram:
 			r"https?:\/\/(?:www\.)?instagram\.com/(reel|p)/([^/\s?]+)",
 			re.IGNORECASE | re.MULTILINE,
 		)
+		self.api = QuickvidsHandler()
 
 	async def get_response(self, user_input: str) -> str | None:
 		try:
@@ -20,8 +27,11 @@ class Instagram:
 				else:
 					raise ValueError("Invalid link type")
 
-				return f"[{type_printed} via Instagram](https://g.ddinstagram.com/{type}/{url_data}/)"
+				try:
+					return await self.api.get_response(match, Service.INSTAGRAM)
+				except (NoAPIResponseError, APIError, aiohttp.ClientError):
+					return f"[{type_printed} via Instagram](https://g.ddinstagram.com/{type}/{url_data}/)"
 		except ValueError as e:
-			print(f"ValueError in Instagrams bot response: {e}")
+			print(f"ValueError in Instagram bot response: {e}")
 		except Exception as e:
-			print(f"Unexpected Error in Instagrams bot response: {e}")
+			print(f"Unexpected Error in Instagram bot response: {e}")

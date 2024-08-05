f95d5a12308a836a7d72943c22a9ddcb829995e7
Suhayb Ullah <75957262+Suhaybu@users.noreply.github.com>
2024-08-05 23:28:19 +0300

    now use a ServiceResponse that stores response content and the service enum

--- models/enums/service.py
+++ models/enums/service.py
@@ -4,3 +4,13 @@ from enum import Enum
 class Service(Enum):
 	TIKTOK = "TikTok"
 	INSTAGRAM = "Instagram"
+	TWITTER = "Twitter"
+
+
+class ServiceResponse:
+	def __init__(self, service: Service, content: str):
+		self.service = service
+		self.content = content
+
+	def __repr__(self):
+		return f"ServiceResponse(service={self.service}, content={self.content})"

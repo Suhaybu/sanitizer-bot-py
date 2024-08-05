997d01135dbbc8317c80a334ffa762305959da8d
Suhayb Ullah <75957262+Suhaybu@users.noreply.github.com>
2024-08-05 21:00:51 +0300

    now uses abstracted quickvids_handler.py instead

--- socialmedia_handlers/tiktok.py
+++ socialmedia_handlers/tiktok.py
@@ -1,8 +1,7 @@
 import re
 
-import aiohttp
-
-from main import get_quick_vids_token
+from models.enums.service import Service
+from utils.quickvids_handler import QuickvidsHandler
 
 
 class Tiktok:
@@ -11,46 +10,13 @@ class Tiktok:
 			r"https?://(?:\w{1,3}\.)?tiktok\.com/[^\/]+\/?\S*",
 			re.IGNORECASE,
 		)
-		self.token = get_quick_vids_token()
-
-	async def get_api_response(
-		self, input_text: str, detailed: bool = False
-	) -> dict | None:
-		url = "https://api.quickvids.app/v2/quickvids/shorturl"
-		headers = {
-			"Content-Type": "application/json",
-			"Authorization": f"Bearer {self.token}",
-		}
-		data = {"input_text": input_text, "detailed": detailed}
-
-		async with aiohttp.ClientSession() as session:
-			async with session.post(url, json=data, headers=headers) as response:
-				if response.status == 500:
-					data["detailed"] = False
-					async with session.post(
-						url, json=data, headers=headers
-					) as retry_response:
-						response = retry_response
-
-				if response.status == 200:
-					return await response.json()
-
-				print(f"Error creating short URL: {response.text}")
-				return None
+		self.api = QuickvidsHandler()
 
 	async def get_response(self, user_input: str) -> str | None:
 		try:
 			match = re.search(self.tiktok_regex, user_input)
 			if match:
-				api_response = await self.get_api_response(match.group(), detailed=True)
-				if api_response is None:
-					raise RuntimeError("No API response")
-				quickvids_url = api_response["quickvids_url"]
-				if api_response["details"] is not None:
-					author_username = api_response["details"]["author"]["username"]
-					return f"[@{author_username} via TikTok]({quickvids_url})"
-				else:
-					return f"[via TikTok]({quickvids_url})"
+				return await self.api.get_response(match, Service.TIKTOK)
 		except RuntimeError as e:
 			print(f"RuntimeError in TikTok API bot response: {e}")
 		except Exception as e:
